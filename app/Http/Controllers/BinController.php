a188ca32912fc4e0cac861be4ed79818b66458a7
Nguyen Nam <nguyen.nam@urban-funes.co.jp>
2017-08-21 19:23:07 +0900

    pull

--- app/Http/Controllers/BinController.php
+++ app/Http/Controllers/BinController.php
@@ -46,13 +46,14 @@ class BinController extends Controller {
 
 		if($username && $password){
 			$git_url = "https://{USERNAME}:{PASSWORD}@urbanfunes.backlog.jp/git/RESERVED/COST.git";
-			$cmd = "git pull {URL}";
+			$cmd = base_path() . "git pull {URL}";
 
 			$git_url = str_replace("{USERNAME}", $username, $git_url);
 			$git_url = str_replace("{PASSWORD}", $password, $git_url);
 			$cmd = str_replace("{URL}", $git_url, $cmd);
-
-			chdir(base_path());
+// echo $cmd;
+// echo getcwd();
+			// chdir(base_path());
 			$result_s = exec($cmd);
 			echo $result_s;
 		}else{
