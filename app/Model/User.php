7221abee8e49c69a3a0db2fd51b36521110fc277
Ngoc Nam <ngocnammoney@gmail.com>
2017-12-23 20:27:42 +0700

    DB table name: user to users

--- app/Model/User.php
+++ app/Model/User.php
@@ -10,44 +10,43 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
 
-    // use Authenticatable, CanResetPassword;
-    use Notifiable;
+	// use Authenticatable, CanResetPassword;
+	use Notifiable;
 
-    protected $guard = 'admin';
+	protected $guard = 'admin';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'id',
-        'email',
-        'password',
-        'name',
-        'session_is_manager',
-        'session_id',
-        // 'gender',
-        // 'birthday',
-        // 'phone',
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'id',
+		'email',
+		'password',
+		'name',
+		'session_is_manager',
+		'session_id',
+		// 'gender',
+		// 'birthday',
+		// 'phone',
 
-        'remember_token',
+		'remember_token',
 
-    ];
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
 
-    protected $table = 'user';
 
-    public function setPasswordAttribute($password)
-    {
-        $this->attributes['password'] = \Hash::make($password);
-    }
+	public function setPasswordAttribute($password)
+	{
+		$this->attributes['password'] = \Hash::make($password);
+	}
 }
