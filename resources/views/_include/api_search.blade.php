52c9037a03a3b51ee0588af2cd148ad59eee8c3b
Ngoc Nam <ngocnammoney@gmail.com>
2018-01-13 17:20:06 +0700

    angularjs

--- resources/views/_include/api_search.blade.php
+++ resources/views/_include/api_search.blade.php
@@ -5,10 +5,10 @@
 		<form method="GET" action="{{ $data['url_pattern'] }}" accept-charset="UTF-8" role="search">
 			<div class="w3-row w3-section">
 				<div class="w3-col s8 m6 l6">
-					<input class="w3-input w3-border" id="keyword" name="keyword" type="text" placeholder="検索キーワード" ng-keypress="loadData()"value="{{ $keyword }}">
+					<input class="w3-input w3-border" id="keyword" name="keyword" type="text" placeholder="検索キーワード" ng-keydown="loadData()"value="{{ $keyword }}">
 				</div>
 				<div class="w3-col s4 m3 l3">
-					<button type="button" class="w3-button w3-brown" ng-click="loadData()"><span class="glyphicon glyphicon-search"></span></button>
+					<button type="button" class="w3-button w3-brown" ng-keydown="loadData()"><span class="glyphicon glyphicon-search"></span></button>
 					<button type="reset" class="w3-button w3-brown"><span class="glyphicon glyphicon-refresh"></span></button>
 				</div>
 			</div>
