dc02ebd7bf90719c7d1177a7620fff7c42b2d28d
Nguyen Nam <nguyen.nam@urban-funes.co.jp>
2018-03-19 10:33:47 +0900

    softDelete

--- app/Model/Project.php
+++ app/Model/Project.php
@@ -2,10 +2,14 @@
 
 namespace App\Model;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+	// https://laravel.com/docs/5.6/eloquent
+	use SoftDeletes;
+
 	protected $fillable = [
 		'id',
 		'organization_id',
@@ -13,6 +17,8 @@ class Project extends Model
 		'description',
 		'is_off',			// '0': FALSE, '1': TRUE
 	];
+
+	protected $dates = ['deleted_at'];
 
 	protected $table = 'project';
 }
