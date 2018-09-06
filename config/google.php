43d3f164e8bde28561aa22a3941beb1ab066e102
Nguyen Nam <nguyen.nam@urban-funes.co.jp>
2018-09-06 10:29:27 +0900

    google api

---
+++ config/google.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'map' => [
+		'client_id'                 => '418174037977-7idgpgt9u2469ddcgeub9hhhg1bsrkic.apps.googleusercontent.com',
+		'client_secret'             => 'PYPARny8RmclaaeE-h43rAxl',
+	],
+
+];
